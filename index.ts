d5ae36127843a9073355f6733d15b9353ed40641
Thomas Perrein <thomas.perrein@student-cs.fr>
2023-10-24 14:19:29 +0200

    test functionality

--- index.ts
+++ index.ts
@@ -1,4 +1,55 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
+import { readLines } from "https://deno.land/std@0.119.0/io/mod.ts";
+import { BufReader } from "https://deno.land/std@0.119.0/io/bufio.ts";
+import { shuffle } from "https://deno.land/std@0.119.0/strings/mod.ts";
+
+const words: string[] = [];
+const wordFilePath = "./list_of_words.txt";
+
+async function loadWords() {
+  const file = await Deno.open(wordFilePath);
+  const bufReader = new BufReader(file);
+  let line: string | null;
+  while ((line = await readLines(bufReader)) !== null) {
+    words.push(line.value);
+  }
+  Deno.close(file.rid);
+}
+
+async function getRandomWord(): Promise<string> {
+  if (words.length === 0) {
+    throw new Error("No words loaded.");
+  }
+  return shuffle(words)[0];
+}
+
+let wordToFind: string | undefined;
+
+async function initializeGame() {
+  await loadWords();
+  wordToFind = await getRandomWord();
+  console.log(`Word to find: ${wordToFind}`);
+}
+
+initializeGame();
+
+async function handler(req: Request): Promise<Response> {
+  try {
+    const guess = await extractGuess(req);
+    if (wordToFind && guess.toLowerCase() === wordToFind.toLowerCase()) {
+      return new Response(`Well played! The word was ${wordToFind}.`);
+    }
+    const similarityResult = await similarity(guess, wordToFind || "");
+    console.log(
+      `Tried with word ${guess}, similarity is ${similarityResult}, word to find is ${wordToFind}`
+    );
+    return new Response(responseBuilder(guess, similarityResult));
+  } catch (e) {
+    console.error(e);
+    return new Response("An error occurred: " + e.message, { status: 500 });
+  }
+}
+
 
 async function handler(_req: Request): Promise<Response> {
   try {
