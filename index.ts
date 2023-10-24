c8d9501d92f5401ab3633d46aa66c96c36938d11
Thomas Perrein <thomas.perrein@student-cs.fr>
2023-10-24 14:01:03 +0200

    add index.ts

---
+++ index.ts
@@ -0,0 +1,8 @@
+
+import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
+
+async function handler(_req: Request): Promise<Response> {
+  return new Response("Hello World");
+}
+
+serve(handler);
