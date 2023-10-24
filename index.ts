8dc3c8a969c2aa8484a2ea4459d45065e1c61fb1
Thomas Perrein <thomas.perrein@student-cs.fr>
2023-10-24 14:29:30 +0200

    fix

--- index.ts
+++ index.ts
@@ -33,6 +33,49 @@ async function initializeGame() {
 
 initializeGame();
 
+const extractGuess = async (req: Request) => {
+const slackPayload = await req.formData();
+const guess = await slackPayload.get("text")?.toString();
+if (!guess) {
+    throw Error("Guess is empty or null");
+}
+return guess;
+};
+
+const responseBuilder = (word: string, similarity: Number) => {
+if (similarity == 1) {
+    return `Well played ! The word was ${word}.`;
+} else if (similarity > 0.5) {
+    return `${word} is very close to the word, score : ${similarity}`;
+} else if (similarity < 0.5) {
+    return `${word} is quite far to the word, score : ${similarity}`;
+}
+};
+
+const similarity = async (word1, word2) => {
+const body = {
+    sim1: word1,
+    sim2: word2,
+    lang: "fr",
+    type: "General Word2Vec",
+};
+console.log("body", body);
+const similarityResponse = await fetch(
+    "http://nlp.polytechnique.fr/similarityscore",
+    {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+    }
+);
+console.log("similarityResponse", similarityResponse);
+const similarityResponseJson = await similarityResponse.json();
+console.log("similarityValue", similarityResponseJson);
+return Number(similarityResponseJson.simscore);
+};
+
 async function handler(req: Request): Promise<Response> {
   try {
     const guess = await extractGuess(req);
@@ -48,65 +91,7 @@ async function handler(req: Request): Promise<Response> {
     console.error(e);
     return new Response("An error occurred: " + e.message, { status: 500 });
   }
+  
+  
 }
-
-
-async function handler(_req: Request): Promise<Response> {
-    try {
-      const wordToFind = "chien";
-      const guess = await extractGuess(_req);
-      const similarityResult = await similarity(guess, wordToFind);
-      console.log(
-        `Tried with word ${guess}, similarity is ${similarityResult}, word to find is ${wordToFind}`
-      );
-      return new Response(responseBuilder(guess, similarityResult));
-    } catch (e) {
-      console.error(e);
-      return new Response("An error occured : ", e);
-    }
-  }
-  
-  const extractGuess = async (req: Request) => {
-    const slackPayload = await req.formData();
-    const guess = await slackPayload.get("text")?.toString();
-    if (!guess) {
-      throw Error("Guess is empty or null");
-    }
-    return guess;
-  };
-  
-  const responseBuilder = (word: string, similarity: Number) => {
-    if (similarity == 1) {
-      return `Well played ! The word was ${word}.`;
-    } else if (similarity > 0.5) {
-      return `${word} is very close to the word, score : ${similarity}`;
-    } else if (similarity < 0.5) {
-      return `${word} is quite far to the word, score : ${similarity}`;
-    }
-  };
-  
-  const similarity = async (word1, word2) => {
-    const body = {
-      sim1: word1,
-      sim2: word2,
-      lang: "fr",
-      type: "General Word2Vec",
-    };
-    console.log("body", body);
-    const similarityResponse = await fetch(
-      "http://nlp.polytechnique.fr/similarityscore",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(body),
-      }
-    );
-    console.log("similarityResponse", similarityResponse);
-    const similarityResponseJson = await similarityResponse.json();
-    console.log("similarityValue", similarityResponseJson);
-    return Number(similarityResponseJson.simscore);
-  };
-  
   serve(handler);
